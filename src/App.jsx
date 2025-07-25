7203cb01d2bf0752ea52d9c1d86c820e317e0f97
techtee19 <techtee19@gmail.com>
2025-07-25 15:30:32 +0100

    Add navigation and layout components with styling
    - Created AppNav component for application navigation.
    - Added AppNav.module.css for styling the navigation.
    - Implemented Logo component with Logo.module.css for logo styling.
    - Developed PageNav component for page navigation, including links to Pricing, Product, and Login.
    - Styled PageNav with PageNav.module.css.
    - Established AppLayout component to wrap application pages.
    - Created Homepage component with introductory content and styled it with Homepage.module.css.
    - Developed Login component with form handling and styled it using Login.module.css.
    - Created PageNotFound component for handling 404 errors.
    - Implemented Pricing and Product components with respective styles.
    - Added City, Country, and Form components for managing travel data.
    - Created reusable UI components like Button, Message, and Spinner with respective styles.
    - Implemented User component for displaying user information and logout functionality.
    - Added global styles in index.css for consistent theming across the application.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,24 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Homepage from "./pages/Homepage.jsx";
+import Pricing from "./pages/Pricing";
+import PageNotFound from "./pages/PageNotFound.jsx";
+import AppLayout from "./pages/AppLayout.jsx";
+import Login from "./pages/Login.jsx";
+import Product from "./pages/Pricing";
+
 function App() {
-  return <div>worldWise</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="product" element={<Product />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="app" element={<AppLayout />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
